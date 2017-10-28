0a47440dda322118c941a069b19a82dbdbca326d
Dennis Durairaj <dennis.durairaj@gmail.com>
2017-10-28 12:33:22 +0200

    Add semantic-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import Home from "./components/pages/Home";
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <div className="ui container">
+      <Route path="/" exact component={Home} />
+    </div>
+  );
 };
 
 export default App;
